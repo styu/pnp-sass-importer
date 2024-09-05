e9d59fff85c726203c8ac736be3f1aac9f4b14fc
Stephanie Yu <styu314@gmail.com>
2024-09-05 17:29:40 -0400

    Detect whether pnpapi is actually available

--- packages/pnp-sass-importer/src/legacyImporter.ts
+++ packages/pnp-sass-importer/src/legacyImporter.ts
@@ -1,5 +1,4 @@
 import { LegacyImporter } from "sass";
-import pnpapi from "pnpapi";
 
 /**
  *
@@ -7,22 +6,35 @@ import pnpapi from "pnpapi";
  * @returns
  */
 export function legacyImporter(dirname: string) {
-    const importer: LegacyImporter = (url, _prev, done) => {
-        let res: string | null = null;
+    const importer: LegacyImporter = async (url, _prev, done) => {
+        let pnpApiPromise;
         try {
-            res = pnpapi.resolveRequest(url, dirname);
+            // If this plugin is not actually run in a PnP context (e.g. nodeLinker is set to node-modules),
+            // this import statement will throw. In that scenario, this importer is actually not necessary
+            // because Sass's --pkg-importer functionality will have node_modules to traverse
+            pnpApiPromise = import("pnpapi");
         } catch (error) {
-            // It's possible the package's exports weren't set up correctly and this URL is attempting to reach into the package nonetheless
-            // In that case, we can see if the URL is simply missing a .scss extension and try again
-            if (!url.endsWith(".scss")) {
-                res = pnpapi.resolveRequest(url + ".scss", dirname);
-            }
-        }
-        if (res == null) {
+            console.log("pnpapi not found, pnp-sass-importer is not needed outside of a PnP context. Returning null");
             done(null);
-        } else {
-            done({ file: res.toString() });
+            return;
         }
+        pnpApiPromise.then(pnpapi => {
+            let res: string | null = null;
+            try {
+                res = pnpapi.resolveRequest(url, dirname);
+            } catch (error) {
+                // It's possible the package's exports weren't set up correctly and this URL is attempting to reach into the package nonetheless
+                // In that case, we can see if the URL is simply missing a .scss extension and try again
+                if (!url.endsWith(".scss")) {
+                    res = pnpapi.resolveRequest(url + ".scss", dirname);
+                }
+            }
+            if (res == null) {
+                done(null);
+            } else {
+                done({ file: res.toString() });
+            }
+        });
     };
     return importer;
 }
